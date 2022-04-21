9e61eabcc2001276074bc014767d6689a6958093
Felipe Zica <felipevzica@gmail.com>
2022-04-21 15:19:27 -0300

    valid english word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,13 @@ class GamesController < ApplicationController
   end
 
   def score
-
+    @word = params[:word]
+    @response = RestClient.get("https://wagon-dictionary.herokuapp.com/#{@word}")
+    @json = JSON.parse(@response)
+   if @json["found"] == true && @word.include?(@letters.to_s)
+      @message = "Congratulations! #{@word} is a valid English word!"
+    else
+      @message = "Sorry but #{@word} does not seem to be a valid English word..."
+    end
   end
 end
