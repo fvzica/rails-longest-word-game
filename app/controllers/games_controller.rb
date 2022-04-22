1beac6d7ec618c6804b2ff6c2f4edb8aff26707f
Felipe Zica <felipevzica@gmail.com>
2022-04-21 21:24:41 -0300

    all done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,12 +6,16 @@ class GamesController < ApplicationController
   end
 
   def score
+    @letters = params[:letters]
     @word = params[:word]
+    @contagem = @word.length == @letters.split.map {|l| @word.include?(l)}.count(true)
     @response = RestClient.get("https://wagon-dictionary.herokuapp.com/#{@word}")
     @json = JSON.parse(@response)
-   if @json["found"] == true && @word.include?(@letters.to_s)
+   if @json["found"] == true && @contagem
       @message = "Congratulations! #{@word} is a valid English word!"
-    else
+   elsif @json["found"] == true && @contagem == false
+    @message = "Valid english word, but not in the random letters =( "
+   else
       @message = "Sorry but #{@word} does not seem to be a valid English word..."
     end
   end
